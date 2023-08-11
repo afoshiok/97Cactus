4b82e72bed17b6a172de29b5d48bd8c82f6bfeef
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2023-08-10 19:52:39 -0500

    Started DB schema

--- api/models.ts
+++ api/models.ts
@@ -1,1 +1,23 @@
 import { Schema } from "mongoose";
+
+interface Creator {
+    id: string;
+    name: string;
+}
+
+interface SubmittedSong {
+    name: string;
+    songLink: string;
+    creator: string;
+    dateSubmitted: Date;
+    artistSampled: string;
+    sampledAlbum: string;
+    albumYear: number;
+    sampledSong: string;
+}
+
+const SongSchema = new Schema<SubmittedSong>({
+    name: { type: String, required: true },
+    creator: { type: String, required: true }
+    //finish schema 
+})
